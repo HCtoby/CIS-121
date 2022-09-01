fe61c4f35aa737e52b65aefe42fb814c397a8aeb
HCtoby <zq2118or@go.minnstate.edu>
2022-09-01 12:51:38 -0500

    1

---
+++ test.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you?"))
+name = str(input("What's your name?"))
+lastname = str(input("What's your lastname?"))
+print("Hi", name, lastname, ", you're", age, "years old, awesome!") 
