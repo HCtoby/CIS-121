69465cddc2a47645bc56f1db41a320a8d11dec91
HCtoby <zq2118or@go.minnstate.edu>
2022-09-08 13:26:35 -0500

    This one is also my code about the Lab 3

--- test.py
+++ test.py
@@ -15,6 +15,8 @@ while status != 1:
                 tex = income / 100 * 12
             elif income <= 172750:
                 tex = income / 100 * 22
+            else:
+                tex = income 
         elif marrystatus == "s":
             if income <= 9950:
                 tex = income / 10
@@ -22,6 +24,8 @@ while status != 1:
                 tex = income / 100 * 12
             elif income <= 86375:
                 tex = income / 100 * 22
+            else:
+                tex = income
         print("According to our tex rate, sence you are", marrystatus, "marry status, you should to pay", tex, "as your tex.")
         status = 1
     else:
