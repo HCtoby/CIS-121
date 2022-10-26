b4beefd4831e953d0a2c5451d1e5135a40c96f2f
HCtoby <zq2118or@go.minnstate.edu>
2022-10-26 13:44:29 -0500

    .

---
+++ example/class_two.py
@@ -0,0 +1,58 @@
+class dog:
+    def __init__(self,name,color,eye_color,height,length,weight,method):
+        self.name = name
+        self.color = color
+        self.eye_color = eye_color
+        self.height = height
+        self.length = length
+        self.weight = weight
+        self.method = method
+    
+    def sit(self):
+        print("I'm sitting.")
+
+    def lay(self):
+        print("I'm laying down.")
+
+
+
+    def __str__(self):
+        print(f"Name: {self.name}")
+        print(f"Color: {self.color}")
+        print(f"Eye color: {self.eye_color}")
+        print(f"Height: {self.height}")
+        print(f"Length: {self.length}")
+        print(f"Weight: {self.weight}")
+        print(f"Method: {self.method}")
+
+        return " "
+
+
+
+
+""
+
+
+
+class animal:
+    def __init__(self,name):
+        self.name = name
+
+    def 
+
+
+
+class cat:
+    def __init__(self,name):
+        self.name = name
+        
+
+
+
+
+
+# Dog
+# name,color,eye_color,height,length,weight,method
+# 
+
+
---
+++ example/main_two.py
@@ -0,0 +1,19 @@
+# from class_two import dog
+
+# fr = dog("Bobby","yellow","brown","17 ih","30 ih","50 g","sit")
+
+# fr.sit
+
+# fr.lay
+
+
+""
+
+
+
+
+from class_two import diagram
+
+
+table = diagram("shape","length",["+get","-set"])
+print(table)
