c2837126788319b14b7c561f99e0ce909d25c8ba
HCtoby <zq2118or@go.minnstate.edu>
2022-11-02 13:54:13 -0500

    .

---
+++ Lab/MyProgram.py
@@ -0,0 +1,16 @@
+"""
+#Name       Haozhe Chen
+#Date       10/23
+
+#What does this do?
+"""
+
+from Lab.ClassFile import RoboFriend
+
+bot1 = RoboFriend("Bob",10)
+bot2 = RoboFriend("Mike",20)
+bot3 = RoboFriend("Alex",30)
+print(bot1)
+print(bot2)
+print(bot3)
+
